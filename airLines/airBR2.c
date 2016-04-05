f12631bd277caec458723e1a0b79e920ab4fa7d8
Zuchis <liquimaster@gmail.com>
2016-04-05 13:34:32 -0300

    strncpy

--- airLines/airBR2.c
+++ airLines/airBR2.c
@@ -11,8 +11,8 @@ int main(){
     char pais[P_T];
     FILE *arq;
     char url[] = "airlines.dat.txt";
-    int c,i,j,teste=0,l,k,falta;
-    int br=0;
+    int controle=0,c,i,j,teste=0,l,k,falta;
+    int m,br=0;
 
     arq = fopen(url,"r");
     if(arq==NULL){
@@ -25,47 +25,53 @@ int main(){
         i=0;
         while(1){
             buffer[MAXBUFFER+1]='\0';
-            if(!falta)j=0;
+            if(!falta){
+                m=0;
+            }
+
             falta=0;
-            
             while(buffer[i++]!='\n'){
                 //info[j++]=buffer[i++];
-		//j++;
+                //j
+                m++;
                 if(i==MAXBUFFER)break;
-            }
-            strncpy(info,&buffer[j],sizeof(char)*i);
-	    //
-		//return 0;
+            } 
+            //
+            //return 0;
             if(i==MAXBUFFER){
                 if(buffer[i]!='\n')falta=1;
                 break;
-            }else j=i;
+            }
+            strncpy(info,&buffer[j],sizeof(char)*i);
             //info[j]='\0';
-	    //printf("%s\n",info);
+            //printf("%s\n",info);
             //return 0;
-	    for(k=0;k<MAXL;k++){
+            for(k=0;k<MAXL;k++){
                 if(info[k]==',')c++;
                 if(c==6)break;
             } 
             k+=2;
             c=0; 
             /*for(l=k;info[l]!='"';l++){
-                pais[c++] = info[l];
-            }*/
-		l=k;
-		int m =0;
-		while(info[l++]!='"'){
-			m++;
-		}
-            	strncpy(pais,&info[k],sizeof(char)*m);
-		printf("%s\n",pais);
-		return 0;
-		//pais[c]='\0';
+              pais[c++] = info[l];
+              }*/
+            l=k;
+            m=0;
+            while(info[l++]!='"'){
+                //                   printf("teste\n");	
+                m++;
+            }    
+            strncpy(pais,&info[k],sizeof(char)*m);	
+            controle++;
+            //return 0;
+            //pais[m+1]='\0';
+            printf("%s\n",pais);
+            if(controle==3)return 0;
             if(strcmp(pais,"Brazil")==0){
                 printf("%s\n",info);
                 br++;
             }
-            i++;
+
             c=0;
         }
     }
