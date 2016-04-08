07d01e9848e6b712cf60309e69e5b1d15b6fe6cd
Udesc <ruan@udesc>
2016-04-08 02:28:21 -0300

    Versão com buffer

--- airLines/airBR2.c
+++ airLines/airBR2.c
@@ -2,7 +2,7 @@
 #include <string.h>
 
 #define MAXBUFFER 8196
-#define MAXL 300
+#define MAXL 1000
 #define P_T 100
 
 int main(){
@@ -20,32 +20,11 @@ int main(){
         return 0;
     }
 
-    /*while(fgets(info, STR_T, arq)!=NULL){
-      c=0;
-      teste++;
-      for(i=0;i<STR_T;i++){
-      if(info[i]==',')c++;
-      if(c==6)break;
-      }
-      i+=2;
-      c=0;
-      for(j=i;info[j]!='"';j++){
-      pais[c++] = info[j];
-      }
-      pais[c]='\0';
-      if(strcmp(pais,"Brazil")==0){
-      printf("%s\n",info);
-      br++;
-      }
-
-      }*/
-    //size_t newLen =  
-    //vIdAtual=0;
     falta=0;
     while(fread(buffer,sizeof(char),MAXBUFFER,arq)!=0){ 
         i=0;
         while(1){
-            buffer[MAXBUFFER]='\0';
+            buffer[MAXBUFFER+1]='\0';
             if(!falta)j=0;
             falta=0;
             
@@ -53,20 +32,20 @@ int main(){
                 info[j++]=buffer[i++];
                 if(i==MAXBUFFER)break;
             }
-
+            
             if(i==MAXBUFFER){
                 if(buffer[i]!='\n')falta=1;
                 break;
             }
             info[j]='\0';
 
-            for(k=0;k<MAXL;i++){
+            for(k=0;k<MAXL;k++){
                 if(info[k]==',')c++;
                 if(c==6)break;
             }
+            
             k+=2;
-            c=0;
-printf("teste\n");
+            c=0; 
             for(l=k;info[l]!='"';l++){
                 pais[c++] = info[l];
             }
@@ -75,12 +54,10 @@ printf("teste\n");
                 printf("%s\n",info);
                 br++;
             }
+            i++;
+            c=0;
         }
-        //break;
     }
-    /*while(size_t newLen = fread(info,sizeof(char),STR_T,arq)!=0){
-      printf("teste\n");  
-      }*/
 
     fclose(arq);
     printf("Quantidade = %i\n",br);
