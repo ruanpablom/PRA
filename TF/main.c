a5329464651672ebe414d56ca8fb66f4a3b5b7fe
ruanpablom <ruan@udesc>
2016-05-17 15:05:17 -0300

    leitura HEXA

--- TF/main.c
+++ TF/main.c
@@ -1,45 +1,75 @@
 #include<stdio.h>
 #include<string.h>
 #include<stdlib.h>
+#include <stdint.h>
 
 #define LIVRO 1
 #define LEITOR 2
 #define AUTOR 3
 #define AUTOR_DO_LIVRO 4
 #define EMPRESTIMO 5
+#define HEXA_CODE 0xF0FA
+#define ARQ_INEX -1
+#define ARQ_NVAL -2
+#define TAM_HEXA_CODE 7
 
 int menu();
-void incluirRegistro(int entidade);
+int incluirRegistro(int entidade);
 char *stringEntidade(int entidade);
 
 //strings globais
 char banco[]        = {"biblioteca-"};
-char livro[]        = {"livro"};
-char leitor[]       = {"leitor"};
-char autor[]        = {"autor"};
-char autorDoLivro[] = {"autorDoLivro"};
-char emprestimo[]   = {"emprestimo"};
+char livro[]        = {"Livro"};
+char leitor[]       = {"Leitor"};
+char autor[]        = {"Autor"};
+char autorDoLivro[] = {"AutorDoLivro"};
+char emprestimo[]   = {"Emprestimo"};
 
 int main(){
-        incluirRegistro(menu());		
-	return 0;
+    incluirRegistro(menu());		
+    return 0;
 }
 
 int menu(){
-	int entidade=0;	
-	printf("1-Livro\n2-Leitor\n3-Autor\n4-Autor Do Livro\n5-Emprestimo\n");
-	while(entidade<1 || entidade>5){
-		printf("Digite o número da entidade que voce deseja operar : ");
-		scanf("%i",&entidade);
-		if(entidade<1 || entidade>5)printf("Digite uma entidade válida!\n");
-	}
-	return entidade;
+    char *e;
+    int entidade=0;	
+    printf("1-Livro\n2-Leitor\n3-Autor\n4-Autor Do Livro\n5-Emprestimo\n");
+    while(entidade<1 || entidade>5){
+        printf("Digite o número da entidade que voce deseja operar : ");
+        scanf("%i",&entidade);
+        if(entidade<1 || entidade>5)printf("Digite uma entidade válida!\n");
+    }
+    system("clear");
+    switch(entidade){
+        case LIVRO:
+            e = livro;
+            break;
+        case LEITOR:
+            e = leitor;
+            break;
+        case AUTOR:
+            e = autor;
+            break;
+        case AUTOR_DO_LIVRO:
+            e = autorDoLivro;
+            break;
+        case EMPRESTIMO:
+            e = emprestimo;
+            break;
+    }
+    printf("Você selecionou a entidade \"%s\"\n",e);
+    return entidade;
 }
 
-void incluirRegistro(int entidade){
+int incluirRegistro(int entidade){
     FILE *arq;
     char *bEntidade;
     int tEntidade;
+    char hexC[TAM_HEXA_CODE];//variavel que receve a string com o valor hexadecimal
+    uint32_t uv=0;//variavel que recebe o valor hexadecimal
+    char *p;  
+    int i;
+
     //nome do arquivo a ser analisado
     switch(entidade){
         case LIVRO:
@@ -65,8 +95,16 @@ void incluirRegistro(int entidade){
     }
     memcpy(bEntidade,banco,sizeof(char)*strlen(banco));
     strcat(bEntidade,stringEntidade(entidade));
-    printf("%s\n",bEntidade);
-    //parei aqui    
+    //printf("%s\n",bEntidade);
+    //nome do arquvio atribuido a string bEntidade
+
+    //Abrir arquivo para inserção do registro
+    arq=fopen(bEntidade,"ra");
+    if(arq==NULL) return ARQ_INEX;
+    fgets(hexC,sizeof(char)*TAM_HEXA_CODE,arq); 
+    uv=strtoul(hexC, &p, 16);
+    if(uv!=HEXA_CODE) return ARQ_NVAL;
+
 }
 
 char *stringEntidade(int entidade){
